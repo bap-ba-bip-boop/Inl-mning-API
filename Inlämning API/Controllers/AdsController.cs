ab7767c91bc994a17e9e7cc3138fab5bd4926717
Calle <calle.be@telia.com>
2022-05-02 16:22:03 +0200

    b

--- Inlämning API/Controllers/AdsController.cs
+++ Inlämning API/Controllers/AdsController.cs
@@ -2,6 +2,7 @@ using Inlämning_API.DTO;
 using Inlämning_API.Model;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Inlämning_API.Controllers
 {
@@ -10,16 +11,18 @@ namespace Inlämning_API.Controllers
     public class AdsController : ControllerBase
     {
         private readonly APIDbContext _context;
+        private readonly DbSet<Advertisement> adverTisementList;
 
         public AdsController(APIDbContext adc)
         {
             _context = adc;
+            adverTisementList = _context.advertisements;
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            return Ok(_context.advertisements.Select(ad => 
+            return Ok(adverTisementList.Select(ad => 
             new AdItemsDTO
             {
                 Id = ad.Id,
@@ -30,7 +33,7 @@ namespace Inlämning_API.Controllers
         [Route("{id}")]
         public IActionResult GetOne(int Id)
         {
-            var advertisement = _context.advertisements.FirstOrDefault( ad => ad.Id == Id);
+            var advertisement = adverTisementList.FirstOrDefault( ad => ad.Id == Id);
             return (advertisement == null) ? NotFound() : Ok(
                 new AdDTO
                 {
@@ -47,7 +50,7 @@ namespace Inlämning_API.Controllers
                 Title = cad.Title,
                 fillerText = cad.fillerText
             };
-            _context.advertisements.Add(newAd);
+            adverTisementList.Add(newAd);
             _context.SaveChanges();
 
             return CreatedAtAction(
@@ -64,7 +67,7 @@ namespace Inlämning_API.Controllers
         [Route("{id}")]
         public IActionResult EditAdvertisement(int Id, EditAdDTO ead)
         {
-            var ad = _context.advertisements.FirstOrDefault(ad => ad.Id == Id);
+            var ad = adverTisementList.FirstOrDefault(ad => ad.Id == Id);
             if (ad == null)
                 return NotFound();
 
@@ -78,11 +81,11 @@ namespace Inlämning_API.Controllers
         [Route("{id}")]
         public IActionResult RemoveAdvertisement(int Id)
         {
-            var ad = _context.advertisements.FirstOrDefault(ad => ad.Id == Id);
+            var ad = adverTisementList.FirstOrDefault(ad => ad.Id == Id);
             if (ad == null)
                 return NotFound();
 
-            _context.advertisements.Remove(ad);
+            adverTisementList.Remove(ad);
             _context.SaveChanges();
 
             return NoContent();
@@ -91,7 +94,7 @@ namespace Inlämning_API.Controllers
         [Route("{id}")]
         public IActionResult PartialUpdateAdvertisement(int Id, [FromBody] JsonPatchDocument<Advertisement> adEntity)
         {
-            var ad = _context.advertisements.FirstOrDefault(ad => ad.Id == Id);
+            var ad = adverTisementList.FirstOrDefault(ad => ad.Id == Id);
             if (ad == null)
                 return NotFound();
 
